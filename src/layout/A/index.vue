6923f6014331b7b5730eda011c7d4a4c3297931c
shoppingzh <xpzheng6@163.com>
2022-08-27 18:20:34 +0800

    layout

--- src/layout/A/index.vue
+++ src/layout/A/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <RouterView />
+  <div class="flex flex-col w-full h-full">
+    <div class="nav h-[50px] bg-b-primary">
+      
+    </div>
+    <div class="content flex-1 h-0 overflow-auto">
+      <RouterView />
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+</style>
--- src/pages/index/router/index.ts
+++ src/pages/index/router/index.ts
@@ -1,8 +1,14 @@
 import router, { addRoutes } from '@/router'
 
 addRoutes([{
+  name: 'Layout',
   path: '/',
-  component: import('@p-index/views/index.vue')
+  component: import('@/layout/A/index.vue')
 }])
+
+router.addRoute('Layout', {
+  path: '',
+  component: import('@p-index/views/index.vue')
+})
 
 export default router
