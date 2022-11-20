2e955933125fd93f65712855f56051c29675e2fb
shoppingzh <xpzheng6@163.com>
2022-11-20 16:03:08 +0800

    feat: inline svg icon

--- src/components/SvgIcon/index.vue
+++ src/components/SvgIcon/index.vue
@@ -1,17 +1,27 @@
 <template>
-  <span class="svg-icon inline-block">
-    <svg aria-hidden="true" class="w-[1em] h-[1em] fill-current">
+  <span v-if="name" class="svg-icon inline-block">
+    <svg aria-hidden="true">
       <use :xlink:href="symbolId" />
     </svg>
   </span>
+  <span v-else v-html="inline" class="svg-icon inline-block"></span>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
 
 interface Props {
-  name: string
+  name: string,
+  inline: string,
 }
 const props = defineProps<Props>()
 const symbolId = computed(() => `#svg-icon/${props.name}`)
 </script>
+
+<style lang="scss" scoped>
+.svg-icon {
+  :deep(svg) {
+    @apply w-[1em] h-[1em] fill-current;
+  }
+}
+</style>
--- src/pages/dev/views/icon.vue
+++ src/pages/dev/views/icon.vue
@@ -11,4 +11,13 @@
     字体图标：
     <i class="icon-home text-6xl text-blue-500" />
   </div>
+  <div>
+    内联SVG图标：
+    <svg-icon :inline="homeSvg" class="text-6xl text-blue-500" />
+  </div>
 </template>
+
+<script setup lang="ts">
+import homeSvg from '@/icons/svg/home.svg?raw'
+console.log(homeSvg)
+</script>
