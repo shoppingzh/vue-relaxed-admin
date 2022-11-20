1cec1b8fce2247dfa825d03ecaeb055d95592506
shoppingzh <xpzheng6@163.com>
2022-11-20 23:02:31 +0800

    feat: router not found config

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,9 @@
 import type { RouteRecordRaw } from 'vue-router'
 
-const routes: RouteRecordRaw[] = []
+const routes: RouteRecordRaw[] = [{
+  path: '/:paths(.*)*',
+  component: () => import('@/views/error/404.vue')
+}]
 
 export default routes
 
