1d158deef246b175dfccf16ddc27bd35b8fb2595
shoppingzh <xpzheng6@163.com>
2022-12-04 21:04:13 +0800

    feat: pc simulator

--- src/pages/app/views/index.vue
+++ src/pages/app/views/index.vue
@@ -1,7 +1,7 @@
 <template>
   <van-button type="primary" size="mini">hello</van-button>
   <van-button type="danger" size="mini">danger</van-button>
-  <div class="text-2xl">
+  <div class="text-2xl text-gray-700">
     缩放屏幕查看文字缩放情况
   </div>
 </template>
