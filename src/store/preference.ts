9ba188b1ac318c92c632a6f943631a8258ba4851
Shopping <xpzheng6@163.com>
2022-12-08 22:45:11 +0800

    feat: support fullscreen

--- src/store/preference.ts
+++ src/store/preference.ts
@@ -1,17 +1,21 @@
 /**
  * 界面公共表现的状态可统一在此维护
  */
-import { useDark, useToggle } from '@vueuse/core';
+import { useDark, useFullscreen, useToggle } from '@vueuse/core';
 import { defineStore } from 'pinia';
 
 export default defineStore('app', () => {
 
   const darkMode = useDark();
   const toggleDarkMode = useToggle(darkMode);
+  const { isFullscreen, toggle: toggleFullscreen } = useFullscreen(document.body);
 
   return {
     darkMode,
-    toggleDarkMode
+    toggleDarkMode,
+
+    isFullscreen,
+    toggleFullscreen,
   };
 
 });
