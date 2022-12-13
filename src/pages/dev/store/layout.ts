46fa635147ef12a7d5910fdd9a6a05a6e0e64bbf
Shopping <xpzheng6@163.com>
2022-12-13 22:41:42 +0800

    feat: menu auto collapse

--- src/pages/dev/store/layout.ts
+++ src/pages/dev/store/layout.ts
@@ -1,9 +1,16 @@
+import { useWindowSize } from '@vueuse/core';
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 export default defineStore('layout', () => {
 
   const isMenuCollapse = ref(false);
+  const { width: windowWidth } = useWindowSize();
+
+  // 自动折叠
+  watch(windowWidth, () => {
+    isMenuCollapse.value = windowWidth.value <= 768;
+  });
 
   return {
     isMenuCollapse,
