25b234b5a4e23c61787fb84b630a4efc2e2f9e86
Shopping <xpzheng6@163.com>
2022-12-13 22:11:53 +0800

    feat: dev demo

--- src/pages/dev/views/global-style.vue
+++ src/pages/dev/views/global-style.vue
@@ -1,6 +1,6 @@
 <template>
   <Example title="文本">
-    我是一段默认文本，我的文字大小是 <Copy text="text-base" />，颜色是 <Copy text="text-regular" />
+    我是一段默认文本，我的文字大小是 <Copy text="text-base" />，颜色是 <Copy text="text-primary" />
   </Example>
 
   <Example title="标题">
