235297a76cb0b853a0da52fc4847159aaf9016b1
Shopping <xpzheng6@163.com>
2022-12-15 21:19:12 +0800

    feat: component locale

--- src/pages/dev/App.vue
+++ src/pages/dev/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <RouterView />
+  <el-config-provider :locale="locale" size="default">
+    <RouterView />
+  </el-config-provider>
 </template>
 
 <script setup lang="ts">
 import { useNetwork } from '@vueuse/core';
 import { watch } from 'vue';
+import locale from 'element-plus/dist/locale/zh-cn.mjs';
 
 const { isOnline } = useNetwork();
 
