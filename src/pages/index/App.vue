583ab739c87c7622ba2ae8c8b081ad70df3da873
Shopping <xpzheng6@163.com>
2022-12-16 13:41:24 +0800

    feat: clean index page

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <RouterView />
+  <div class="p-4 text-h5">
+    <div>暂时是一个空白的单页，项目的细节可前往 <a href="dev.html">开发页</a> 查看！</div>
+  </div>
 </template>
 
 <script setup lang="ts">
--- src/pages/index/main.ts
+++ src/pages/index/main.ts
@@ -1,17 +1,12 @@
 import '@/main';
 import { createApp } from 'vue';
-import router from './router';
 import App from './App.vue';
-import '@/permission';
-import { createPinia } from 'pinia';
 import ElementPlus from '@/plugins/element-plus';
 import components from '@/components';
 
 import './styles/index.scss';
 
 createApp(App)
-  .use(router)
-  .use(createPinia())
   .use(ElementPlus)
   .use(components)
   .mount('#app');
