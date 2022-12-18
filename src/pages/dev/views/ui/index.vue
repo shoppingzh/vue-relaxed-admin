4f268c5edf798a261f777fcf352fec0460bad905
Shopping <xpzheng6@163.com>
2022-12-18 16:22:41 +0800

    feat: dev demo

--- src/pages/dev/views/ui/index.vue
+++ src/pages/dev/views/ui/index.vue
@@ -11,7 +11,7 @@
         <div
           v-for="(cls, idx) in color.list"
           :key="cls"
-          class="p-4 transition-all duration-150"
+          class="p-4 transition-all duration-150 truncate"
           :class="[cls, { 'text-gray-200': idx > 6, 'text-gray-300': idx <= 6 && idx > 3 }]">
           <Copy :text="cls" />
         </div>
@@ -157,6 +157,12 @@ const colors = [{
 }, {
   name: 'red',
   list: ['bg-red-100', 'bg-red-200', 'bg-red-300', 'bg-red-400', 'bg-red-500', 'bg-red-600', 'bg-red-700', 'bg-red-800', 'bg-red-900', 'bg-red-1000']
+}, {
+  name: 'pink',
+  list: ['bg-pink-100', 'bg-pink-200', 'bg-pink-300', 'bg-pink-400', 'bg-pink-500', 'bg-pink-600', 'bg-pink-700', 'bg-pink-800', 'bg-pink-900', 'bg-pink-1000']
+}, {
+  name: 'cyan',
+  list: ['bg-cyan-100', 'bg-cyan-200', 'bg-cyan-300', 'bg-cyan-400', 'bg-cyan-500', 'bg-cyan-600', 'bg-cyan-700', 'bg-cyan-800', 'bg-cyan-900', 'bg-cyan-1000']
 }, {
   name: 'gray',
   list: ['bg-gray-100', 'bg-gray-200', 'bg-gray-300', 'bg-gray-400', 'bg-gray-500', 'bg-gray-600', 'bg-gray-700', 'bg-gray-800', 'bg-gray-900', 'bg-gray-1000']
