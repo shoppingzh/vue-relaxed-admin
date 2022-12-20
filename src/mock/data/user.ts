e8a531ba0bf1a8ce77855dd48f0f14266ef60a86
Shopping <xpzheng6@163.com>
2022-12-20 18:07:30 +0800

    feat(base): add mock

---
+++ src/mock/data/user.ts
@@ -0,0 +1,12 @@
+import * as Mock from 'mockjs';
+
+export default [{
+  url: '/api/users',
+  data: (mock: Mock.MockjsMock) => mock({
+    'list|1-100': [{
+      // 属性 id 是一个自增数，起始值为 1，每次增 1
+      name: '@cname',
+      age: '@integer(0, 140)'
+    }]
+  }).list
+}];
