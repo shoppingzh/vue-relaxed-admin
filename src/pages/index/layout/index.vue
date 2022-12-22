13672d79cbbf776e8f45dec87ce042ec12f6c7ec
Shopping <xpzheng6@163.com>
2022-12-22 22:28:11 +0800

    feat(admin): settings ui, width

--- src/pages/index/layout/index.vue
+++ src/pages/index/layout/index.vue
@@ -6,7 +6,7 @@
       <Main class="flex-1 h-0 w-full bg-gray-200" />
     </section>
   </section>
-  <el-drawer v-model="isSetting" size="20%" title="设置">
+  <el-drawer v-model="isSetting" size="300px" title="设置">
     <Settings />
   </el-drawer>
 </template>
