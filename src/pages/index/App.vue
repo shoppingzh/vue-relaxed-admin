7d8188f7f9793288e11b93881b67e454833d9f19
Shopping <xpzheng6@163.com>
2022-12-26 20:35:06 +0800

    feat(admin): locale config

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <RouterView />
+  <el-config-provider :locale="zhCn">
+    <RouterView />
+  </el-config-provider>
 </template>
 
 <script setup lang="ts">
 import usePrefs from '@/store/preference';
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
 
 const prefs = usePrefs();
 prefs.darkMode = false;
