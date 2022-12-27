8b2e03d36a84bee2a3c9c14fc69fea286487326b
Shopping <xpzheng6@163.com>
2022-12-27 11:54:26 +0800

    feat(admin): main area back top

--- src/pages/index/layout/Main.vue
+++ src/pages/index/layout/Main.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="w-full h-full overflow-auto">
+  <div id="main" class="w-full h-full overflow-auto">
     <RouterView />
+    <el-backtop target="#main" />
   </div>
 </template>
