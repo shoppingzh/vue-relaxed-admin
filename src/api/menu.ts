25cfa1739521cf2e31bb86336b2c5284c63fb96d
Shopping <xpzheng6@163.com>
2022-12-27 15:02:35 +0800

    feat(base): menu api

---
+++ src/api/menu.ts
@@ -0,0 +1,9 @@
+import service from '@/service';
+
+export function list() {
+  return service({
+    url: 'menus',
+    method: 'get'
+  });
+}
+
---
+++ src/mock/data/menu.ts
@@ -0,0 +1,26 @@
+const menus: any[] = [{
+  name: '控制台',
+  icon: 'home',
+  path: '/',
+}, {
+  path: '/icon',
+  icon: 'tubiao',
+  name: '图标',
+
+}, {
+  name: '错误页',
+  icon: 'xiayu',
+  children: [{
+    path: '/404',
+    name: '404'
+  }]
+}, {
+  name: '外部链接',
+  icon: 'github',
+  path: 'https://github.com/shoppingzh/vue-template'
+}];
+
+export default [{
+  url: '/api/menus',
+  data: () => menus
+}];
