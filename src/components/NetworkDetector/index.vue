be16c1d751b097afa88331fc9f51435b13e44806
Shopping <xpzheng6@163.com>
2022-12-27 11:51:49 +0800

    feat(admin): ui

--- src/components/NetworkDetector/index.vue
+++ src/components/NetworkDetector/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="!isOnline" class="p-2 bg-b-danger-hover text-white dark:text-black text-center text-xs">
+  <div v-if="!isOnline" class="p-2 bg-b-danger-hover text-white text-center text-xs">
     <svg-icon name="duankai" class="text-h6" /> 网络已断开！请检查网络设置~~
   </div>
 </template>
