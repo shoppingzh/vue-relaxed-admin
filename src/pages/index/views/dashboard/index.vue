5522d0fb4b2236ab70fa67d1353017dd79c2df8a
Shopping <xpzheng6@163.com>
2023-01-02 18:23:01 +0800

    feat(admin): dashboard chart test

--- src/pages/index/views/dashboard/index.vue
+++ src/pages/index/views/dashboard/index.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="p-4 h-[3000px]">
-    控制台
+    <div ref="el" class="w-full h-[300px]" />
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import useChart from '@/hooks/useChart';
+import { EChartsOption } from 'echarts';
+import { computed } from 'vue';
 
+const options = computed<EChartsOption>(() => ({
+  xAxis: {
+    type: 'category'
+  },
+  yAxis: {},
+  tooltip: {},
+  series: [{
+    type: 'bar',
+    data: new Array(50).fill(null).map(() => Math.random() * 100)
+  }]
+}));
+const { el } = useChart(options);
 </script>
