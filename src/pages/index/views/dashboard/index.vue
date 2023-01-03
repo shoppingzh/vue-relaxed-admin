b27bf31c41e4eb345476a319149fe1dd9a95d3ef
Shopping <xpzheng6@163.com>
2023-01-03 12:01:37 +0800

    chore(admin): dashboard

--- src/pages/index/views/dashboard/index.vue
+++ src/pages/index/views/dashboard/index.vue
@@ -1,6 +1,27 @@
 <template>
-  <div class="p-4 h-[3000px]">
-    <div ref="el" class="w-full h-[300px]" />
+  <div class="p-4">
+    <div class="flex">
+      <div
+        v-for="x in 4"
+        :key="x"
+        :class="{ 'ml-2': x > 1 }"
+        class="relative flex items-center flex-1 bg-normal p-2 rounded-sm">
+        <div class="flex items-center justify-center w-[44px] h-[44px] rounded-full bg-gray-200">
+          <svg-icon name="github" class="text-[32px]" />
+        </div>
+        <div class="ml-6">
+          <div class="text-h3">480</div>
+          <div class="">所有阅读数</div>
+        </div>
+        <div class="absolute right-4 top-4 cursor-pointer">
+          <svg-icon name="gengduo" class="text-h6" />
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-3 bg-normal rounded-sm">
+      <div ref="el" class="h-[350px]" />
+    </div>
   </div>
 </template>
 
