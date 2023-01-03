2adbfd5ff8ab7576339fea84feb210174b6a17c0
Shopping <xpzheng6@163.com>
2023-01-03 16:31:05 +0800

    fix(admin): layout, header collapse hover style

--- src/pages/index/layout/Header.vue
+++ src/pages/index/layout/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="flex items-center h-[52px] leading-[52px]">
-    <div v-if="asideCollapseTogglePlace === 'header'" class="h-full flex items-center px-4 cursor-pointer hover:bg-gray-300" @click="isAsideCollapse = !isAsideCollapse">
+    <div v-if="asideCollapseTogglePlace === 'header'" class="h-full flex items-center px-4 cursor-pointer hover:bg-gray-200" @click="isAsideCollapse = !isAsideCollapse">
       <svg-icon name="zhedie" class="text-h5 transition-all duration-300" :class="{ 'rotate-180': isAsideCollapse }" />
     </div>
     <!-- <div class="px-4 text-h6 font-semibold">Hello, Xpzheng!</div> -->
