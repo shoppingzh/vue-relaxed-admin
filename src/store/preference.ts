57fa77ab98da7d161b138010e4af11bfd034f7bc
Shopping <xpzheng6@163.com>
2023-01-04 16:04:44 +0800

    fix(base): prefs, brand color persistent

--- src/store/preference.ts
+++ src/store/preference.ts
@@ -4,7 +4,7 @@
  */
 import { useDark, useFullscreen, useLocalStorage, useToggle } from '@vueuse/core';
 import { defineStore } from 'pinia';
-import { ref, watch } from 'vue';
+import { watch } from 'vue';
 import { changeBrandCssVar } from '@/utils/theme';
 
 export default defineStore('preference', () => {
@@ -16,12 +16,13 @@ export default defineStore('preference', () => {
   const { isFullscreen, toggle: toggleFullscreen } = useFullscreen(document.body);
   const grayMode = useLocalStorage('grayMode', false, {});
   const weakMode = useLocalStorage('weakMode', false);
-  const brandColor = ref<string>();
+  const brandColor = useLocalStorage('brandColor', null);
 
   watch([grayMode, weakMode], updateBodyFilter, { immediate: true });
   watch(brandColor, () => {
+    if (!brandColor.value) return;
     changeBrandCssVar('primary', brandColor.value);
-  });
+  }, { immediate: true });
 
   function updateBodyFilter() {
     const filters = [];
