a23b368119c49d1fc51c5713b53d26858248f501
Shopping <xpzheng6@163.com>
2023-01-04 16:11:50 +0800

    fix(admin): remove bug code

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -5,9 +5,5 @@
 </template>
 
 <script setup lang="ts">
-import usePrefs from '@/store/preference';
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
-
-const prefs = usePrefs();
-prefs.darkMode = false;
 </script>
