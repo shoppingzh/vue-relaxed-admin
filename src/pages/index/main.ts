a9f7b748358d466201996a98921ed3e56e16b4c6
Shopping <xpzheng6@163.com>
2023-02-26 18:50:39 +0800

    feat: add error report plugin

--- src/pages/index/main.ts
+++ src/pages/index/main.ts
@@ -6,9 +6,19 @@ import ElementPlus from '@/plugins/element-plus'
 import components from '@/components'
 import router from './router'
 import pageComponents from './components'
+import errorReport from '@/plugins/error-report'
 import './permission'
 import '@/mock'
 
 import './styles/index.scss'
 
-createApp(App).use(router).use(createPinia()).use(ElementPlus).use(components).use(pageComponents).mount('#app')
+const app = createApp(App)
+
+app
+  .use(router)
+  .use(createPinia())
+  .use(ElementPlus)
+  .use(components)
+  .use(pageComponents)
+  .use(errorReport)
+  .mount('#app')
---
+++ src/plugins/error-report/index.ts
@@ -0,0 +1,7 @@
+import { App } from 'vue'
+
+export default function(app: App) {
+  // app.config.warnHandler = (msg, ins, trace) => {
+  //   console.warn(msg + trace)
+  // }
+}
