95cf58cf269b90b2634b4b7a884ced940fdb8f97
Shopping <xpzheng6@163.com>
2023-02-28 21:45:22 +0800

    feat: set dayjs locale

--- src/plugins/dayjs/index.ts
+++ src/plugins/dayjs/index.ts
@@ -1,5 +1,8 @@
 import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
 import { App } from 'vue'
+
+dayjs.locale('zh-cn')
 
 export default function(app: App) {
   app.config.globalProperties.$dayjs = dayjs
