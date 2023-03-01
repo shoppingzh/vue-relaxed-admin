7e73d6d624871091d7afb0bda50677b97602c189
shoppingzh <xpzheng6@163.com>
2023-03-01 18:38:57 +0800

    feat: page route animation

--- src/pages/index/layout/Main.vue
+++ src/pages/index/layout/Main.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="main" class="w-full h-full overflow-auto">
-    <RouterView />
+    <Transition name="fade">
+      <RouterView />
+    </Transition>
     <el-backtop target="#main" />
   </div>
 </template>
+
+<style scoped>
+.fade {
+  &-enter-from, &-leave-to {
+    @apply translate-x-[-30px] opacity-0;
+  }
+  &-enter-active, &-leave-active {
+    @apply transition-all duration-500;
+  }
+}
+</style>
