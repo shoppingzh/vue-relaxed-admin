c1ae83891f4281926ce3abc0eb5c86909840b2de
shoppingzh <xpzheng6@163.com>
2023-03-02 10:51:22 +0800

    fix(base): update commitlint rules

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,9 @@
+/** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'style', 'refactor', 'build', 'docs']],
+    'scope-enum': [2, 'always', ['base', 'admin']],
+    'scope-empty': [2, 'never'],
+  },
 }
