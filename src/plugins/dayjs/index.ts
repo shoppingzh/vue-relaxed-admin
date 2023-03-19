80aa8b72d4c49375e945a6ee1f29be8dd2770de0
Shopping <xpzheng6@163.com>
2023-03-19 11:01:12 +0800

    feat: dayjs types

--- src/plugins/dayjs/index.ts
+++ src/plugins/dayjs/index.ts
@@ -2,6 +2,12 @@ import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn'
 import { App } from 'vue'
 
+declare module 'vue' {
+  interface ComponentCustomProperties {
+    $dayjs: typeof dayjs
+  }
+}
+
 dayjs.locale('zh-cn')
 
 export default function(app: App) {
