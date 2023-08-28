4b40391c9cd88bd7b9c35d76c67586312dad6f22
Shopping <xpzheng6@163.com>
2023-08-28 21:58:58 +0800

    feat(admin): aside menu tooltip

--- src/pages/index/layout/components/MenuItem.vue
+++ src/pages/index/layout/components/MenuItem.vue
@@ -1,21 +1,30 @@
 <template>
-  <template v-if="item.children && item.children.length">
-    <el-sub-menu>
-      <template #title>
+  <ElTooltip
+    :disabled="!isAsideCollapse && depth > 0"
+    :content="item.name"
+    :placement="hasChild ? 'top' : 'right'"
+    :enterable="false">
+    <template v-if="hasChild">
+      <el-sub-menu>
+        <template #title>
+          <MenuContent :icon="item.icon" :title="item.name" />
+        </template>
+        <MenuItem v-for="child in item.children" :key="child.path" :item="child" :depth="depth + 1" />
+      </el-sub-menu>
+    </template>
+    <template v-else>
+      <el-menu-item :index="item.path" @click="click">
         <MenuContent :icon="item.icon" :title="item.name" />
-      </template>
-      <MenuItem v-for="child in item.children" :key="child.path" :item="child" />
-    </el-sub-menu>
-  </template>
-  <template v-else>
-    <el-menu-item :index="item.path" @click="click">
-      <MenuContent :icon="item.icon" :title="item.name" />
-    </el-menu-item>
-  </template>
+      </el-menu-item>
+    </template>
+  </ElTooltip>
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
 import MenuContent from './MenuContent.vue'
+import useLayout from '@p-index/store/layout'
+import { computed } from 'vue'
 
 interface Item {
   path: string
@@ -25,9 +34,14 @@ interface Item {
 }
 interface Props {
   item: Item
+  depth?: number
 }
-const props = defineProps<Props>()
+const props = withDefaults(defineProps<Props>(), {
+  depth: 0
+})
 const emit = defineEmits(['click'])
+const { isAsideCollapse } = storeToRefs(useLayout())
+const hasChild = computed(() => props.item.children && props.item.children.length)
 
 function click() {
   emit('click', props.item)
