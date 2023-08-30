f642cd9af435602978ac7412091e98d43562f0f8
Shopping <xpzheng6@163.com>
2023-08-30 22:25:38 +0800

    feat(admin): watermark

--- src/pages/index/views/watermark/index.vue
+++ src/pages/index/views/watermark/index.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
-    <div class="content-wrap p-4">
-      水印
-    </div>
-  </div>
+  <div ref="el" class="h-full bg-white dark:bg-black" />
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { createWatermark } from '@shoppingzh/tools/lib/dom'
+
+const el = ref<HTMLElement>()
+
+function render() {
+  createWatermark(el.value, 'Relaxed Admin', {
+    textColor: 'rgba(100, 100, 100, .4)',
+    textSize: 14,
+  })
+}
+
+onMounted(render)
+
+</script>
