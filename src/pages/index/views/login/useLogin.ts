f21931758697f4ae3104831345a43de28529fd98
shoppingzh <xpzheng6@163.com>
2023-09-06 17:32:52 +0800

    feat(admin): remember

--- src/pages/index/views/login/useLogin.ts
+++ src/pages/index/views/login/useLogin.ts
@@ -1,5 +1,6 @@
-import { MaybeRef } from '@vueuse/core'
-import { reactive, ref } from 'vue'
+import { MaybeRef, useLocalStorage } from '@vueuse/core'
+import { merge } from 'lodash'
+import { reactive, ref, watch } from 'vue'
 
 interface Options {
   username?: MaybeRef<string>
@@ -7,18 +8,35 @@ interface Options {
   remember?: MaybeRef<boolean>
 }
 
+interface LoginData {
+  username: string
+  password: string
+}
+
 export default function(options: Options = {}) {
   const username = ref(options.username)
   const password = ref(options.password)
   const remember = ref(options.remember)
+  const store = useLocalStorage<LoginData>('login-data', {} as LoginData)
 
-  const form = reactive({
-    username,
-    password,
+  const form = reactive<LoginData>({
+    username: username as any,
+    password: password as any,
   })
+
+  function toStore() {
+    store.value = form
+  }
+
+  watch(store, () => {
+    merge(form, store.value)
+  }, { immediate: true })
+
 
   return {
     form,
     remember,
+
+    toStore,
   }
 }
