916a15eb4e0424186c8e7b1a3e7b6a90138066d4
shoppingzh <xpzheng6@163.com>
2023-09-22 11:24:53 +0800

    feat: useAutoFocus, set timeout 200

--- src/hooks/useAutoFocus.ts
+++ src/hooks/useAutoFocus.ts
@@ -4,7 +4,7 @@ interface Focusable {
   focus: () => void
 }
 
-export default function(initialInput?: Ref<Focusable>, timeout = 100) {
+export default function(initialInput?: Ref<Focusable>, timeout = 200) {
   const input = initialInput || ref<Focusable>()
 
   function focus() {
