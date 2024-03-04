df41118a0e7aa003d8f2004fe547043013ec6d47
shoppingzh <xpzheng6@163.com>
2024-03-04 14:27:29 +0800

    feat(base): upgrade vite

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,7 +3,5 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', ['feat', 'fix', 'style', 'refactor', 'build', 'docs']],
-    'scope-enum': [2, 'always', ['base', 'admin', 'dev']],
-    'scope-empty': [2, 'never'],
   },
 }
