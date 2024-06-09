fbf8154dcd6200ba4407ee63954a2dde73b8e870
Shopping <xpzheng6@163.com>
2024-06-09 21:58:42 +0800

    feat(base): visualizer plugin

--- build/vite.config.ts
+++ build/vite.config.ts
@@ -8,71 +8,77 @@ import jsx from '@vitejs/plugin-vue-jsx'
 import multiPageRewritePlugin from './multi-page-rewrite-plugin'
 import legacy from '@vitejs/plugin-legacy'
 import compression from 'vite-plugin-compression'
+import { visualizer, } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // 开发相关
-  envPrefix: 'APP_',
-  resolve: {
-    alias: config.pages.reduce(
-      (map, o) => {
-        map[`@p-${o.name}`] = path.resolve(config.rootDir, `src/pages/${o.name}`)
-        return map
-      },
-      {
-        '@': path.resolve(config.rootDir, 'src'),
-      }
-    ),
-  },
-  server: {
-    port: 5173,
-    open: true,
-    proxy: {
-      '/api/': {
-        target: 'http://localhost:3000/',
-        changeOrigin: true,
-        ws: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode, }) => {
+  const isProdMode = mode === 'production'
+  return {
+    // 开发相关
+    envPrefix: 'APP_',
+    resolve: {
+      alias: config.pages.reduce(
+        (map, o) => {
+          map[`@p-${o.name}`] = path.resolve(config.rootDir, `src/pages/${o.name}`)
+          return map
+        },
+        {
+          '@': path.resolve(config.rootDir, 'src'),
+        }
+      ),
+    },
+    server: {
+      port: 5173,
+      open: true,
+      proxy: {
+        '/api/': {
+          target: 'http://localhost:3000/',
+          changeOrigin: true,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
 
-  plugins: [
-    // 多页应用路径重写（注：index单页不需要重写）
-    config.multiPageRewrite && multiPageRewritePlugin({
-      rewrites: config.pages.filter(o => o.name !== 'index').map(o => ({
-        from: new RegExp(`^\\/mpa\\/${o.name}`),
-        to: `/${o.name}.html`,
-      })),
-    }),
-    vue(),
-    jsx(),
-    legacy({
-      // 根据情况设置自己的浏览器版本，或在根目录下新建.browserslistrc文件设置
-      // 为了让其他插件共享配置，更推荐配置文件的方式
-      // targets: ['chrome>=96'],
-    }),
-    // SVG雪碧图
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(config.rootDir, 'src/icons/svg')],
-      symbolId: 'svg-icon/[name]',
-    }),
-    // vue defineXXX支持
-    DefineOptions(),
-    // gzip压缩，保证运行时性能
-    compression({
-      algorithm: 'gzip',
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      // 多页支持
-      input: config.pages.reduce((map, { name, }) => {
-        map[name] = path.resolve(config.rootDir, `${name}.html`)
-        return map
-      }, {}),
+    plugins: [
+      // 多页应用路径重写（注：index单页不需要重写）
+      config.multiPageRewrite && multiPageRewritePlugin({
+        rewrites: config.pages.filter(o => o.name !== 'index').map(o => ({
+          from: new RegExp(`^\\/mpa\\/${o.name}`),
+          to: `/${o.name}.html`,
+        })),
+      }),
+      vue(),
+      jsx(),
+      legacy({
+        // 根据情况设置自己的浏览器版本，或在根目录下新建.browserslistrc文件设置
+        // 为了让其他插件共享配置，更推荐配置文件的方式
+        // targets: ['chrome>=96'],
+      }),
+      // SVG雪碧图
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(config.rootDir, 'src/icons/svg')],
+        symbolId: 'svg-icon/[name]',
+      }),
+      // vue defineXXX支持
+      DefineOptions(),
+      // gzip压缩，保证运行时性能
+      isProdMode && compression({
+        algorithm: 'gzip',
+      }),
+      // 打包后分析依赖图
+      isProdMode && visualizer({})
+    ],
+    build: {
+      rollupOptions: {
+        // 多页支持
+        input: config.pages.reduce((map, { name, }) => {
+          map[name] = path.resolve(config.rootDir, `${name}.html`)
+          return map
+        }, {}),
+      },
+      minify: 'terser',
+      sourcemap: true,
     },
-    minify: 'terser',
-    sourcemap: true,
-  },
+  }
 })
