edf1664daf168ad7d7dcf735c33887a397c2c1f3
Shopping <xpzheng6@163.com>
2024-08-09 23:29:50 +0800

    feat(admin): remove legacy plugin

--- build/vite.config.ts
+++ build/vite.config.ts
@@ -6,7 +6,7 @@ import config from './config'
 import DefineOptions from 'unplugin-vue-define-options/vite'
 import jsx from '@vitejs/plugin-vue-jsx'
 import multiPageRewritePlugin from './multi-page-rewrite-plugin'
-import legacy from '@vitejs/plugin-legacy'
+// import legacy from '@vitejs/plugin-legacy'
 import compression from 'vite-plugin-compression'
 import { visualizer, } from 'rollup-plugin-visualizer'
 import cdn from 'vite-plugin-cdn-import'
@@ -53,11 +53,11 @@ export default defineConfig(({ mode, }) => {
       }),
       vue(),
       jsx(),
-      legacy({
-        // 根据情况设置自己的浏览器版本，或在根目录下新建.browserslistrc文件设置
-        // 为了让其他插件共享配置，更推荐配置文件的方式
-        // targets: ['chrome>=96'],
-      }),
+      // legacy({
+      //   // 根据情况设置自己的浏览器版本，或在根目录下新建.browserslistrc文件设置
+      //   // 为了让其他插件共享配置，更推荐配置文件的方式
+      //   // targets: ['chrome>=96'],
+      // }),
       // SVG雪碧图
       createSvgIconsPlugin({
         iconDirs: [path.resolve(config.rootDir, 'src/icons/svg')],
